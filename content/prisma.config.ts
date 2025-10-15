301c09b1ec870681031a9562ec6aa610f6c3d40c
Fadi Hanna <fadialias230@gmail.com>
2025-10-15 23:21:29 +0200

    New Docker config for all modules

--- content/prisma.config.ts
+++ content/prisma.config.ts
@@ -1,5 +1,9 @@
 import { PrismaConfig } from 'prisma/config';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? './.docker.env' : './.env',
+});
 
 export default {
   schema: './src/prisma/schema.prisma',
